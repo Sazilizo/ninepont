56117b760a4cca956dd9d54fa5e9d98fe70c1462
unknown <lizosazi@gmail.com>
2024-08-09 20:23:13 +0200

    added section 4 to 5

--- js/index.js
+++ js/index.js
@@ -2,26 +2,43 @@ const openBtn = document.getElementById("open_nav");
 const closeBtn= document.getElementById("close_nav");
 const navContainer = document.getElementById("navigation_container");
 const navigation = document.getElementById("navigation_items");
+const dropbtn = document.querySelector(".dropbtn");
+const dropdownContent = document.querySelector(".dropdown-content");
+const overlay = document.getElementById("overlay");
+const overlayText = document.getElementById("overlay-text");
 
 openBtn.addEventListener("click", ()=>{
-    const list = navigation.classList;
-    if(String(list).includes("navigation_items")){
-        navigation.style.display = "block"
-    };
+
+    navigation.classList.remove("closed_menu")
+    navigation.classList.add("navigation_items");
     openBtn.style.display = "none";
     closeBtn.style.display = "block";
-    navContainer.style.backgroundColor= "black";
-    navContainer.style.color = "white";
 });
 
 closeBtn.addEventListener("click", ()=>{
-    const list = navigation.classList;
-    if(String(list).includes("navigation_items")){
-        navigation.style.display = "none"
-    };
+    navigation.classList.remove("navigation_items");
+    navigation.classList.add("closed_menu");
     openBtn.style.display = "block";
     closeBtn.style.display = "none";
-    navContainer.style.backgroundColor= "white";
-    navContainer.style.color = "black";
 });
+
+dropbtn.addEventListener("click", () => {
+    dropdownContent.classList.toggle("show");
+});
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        if (dropdownContent.classList.contains('show')) {
+            dropdownContent.classList.remove('show');
+        }
+    }
+}
+dropdownItems.forEach(item => {
+    item.addEventListener("click", (event) => {
+        event.preventDefault(); 
+        overlayText.textContent = item.textContent; 
+        overlay.classList.add("active");
+    });
+});
+
 
